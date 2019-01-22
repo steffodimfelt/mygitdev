ab9cc63ad87b1a16c9c94fd30d3ae62b95fa7559
Steffo <steffo.dimfelt@gmail.com>
2019-01-22 22:36:16 +0100

    Update index.js
    Remove a console.log

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 //Hela exoport cons = Action Creator
 export const selectCase = (caseItem) => {
   //Det som returneras är en Action
- console.log('Du valde: ', caseItem.headline);
+
   return {
     type: 'SELECT_CASE',
     payload: caseItem

48e6b8dd1d73d49ef212c4421d1cd597bc0609ea
Steffo Dimfelt <steffo.dimfelt@gmail.com>
2020-03-20 08:30:58 +0100

    Set first page background image

--- src/App.js
+++ src/App.js
@@ -16,14 +16,11 @@ class App extends Component {
 
     return (
       <div className='col'>
-        <div className='row'>
-          <div id='hero' className='f-s12 h-20-rem purple-bg'></div>
+        <div className='row '>
+          <div id='hero' className='full-screen overflow-hidden' ><img className="center-center img-full-screen img-center-adjust" src='https://raw.githubusercontent.com/steffodimfelt/steffodimfelt.github.io/master/static/img/cases/web/2x1-DoIt-02.jpg' /></div>
+          
         </div>
-        <div className='row'>
-          <div className='f-s0 f-l2'></div>
-          <div id='title' className='f-s8' style={{height: '200rem', backgroundColor:'red'}}>Steffos portfolio</div>
-          <div className='f-s0 f-l2'></div>
-        </div>
+        
         {openMenu ? <CaseMenu /> : null }
        {/* <div id="InnerContainer" className='row'>
       <CasesList style={{flex: 4, top: 0}} />
@@ -34,7 +31,7 @@ class App extends Component {
 
       <div id='menu' 
       style={{width: '4rem', height: '4rem', borderRadius: '50%', position: 'fixed', textAlign: 'center'}}
-      className='m-s24 pointer purple-bg'
+      className='m-s24 pointer purple-bg shadow-1'
       onClick={toggleMenu}
       >
         <h2 style={{color: 'white', verticalAlign: 'middle', lineHeight: '1.5rem', fontSize:'1.5rem'}}>+</h2>

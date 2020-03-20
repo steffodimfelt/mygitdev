faeaa76a49d5d9bd286796cae9defff64c4d6d62
Steffo Dimfelt <steffo.dimfelt@gmail.com>
2020-03-20 15:01:25 +0100

    First attempt make a header

--- src/App.js
+++ src/App.js
@@ -17,41 +17,74 @@ class App extends Component {
     return (
       <div className='col'>
         <div className='row '>
-          <div id='hero' className='full-screen overflow-hidden' ><img className="center-center img-full-screen img-center-adjust" src='https://raw.githubusercontent.com/steffodimfelt/steffodimfelt.github.io/master/static/img/cases/web/2x1-DoIt-02.jpg' /></div>
+          <div id='hero' className='full-screen overflow-hidden' >
+            <img className="center-center img-full-screen img-center-adjust" src='https://raw.githubusercontent.com/steffodimfelt/steffodimfelt.github.io/master/static/img/cases/3d/BoC-01.jpg' />
+          </div>
           
         </div>
         
         {openMenu ? <CaseMenu /> : null }
-       {/* <div id="InnerContainer" className='row'>
+        {/* <div id="InnerContainer" className='row'>
       <CasesList style={{flex: 4, top: 0}} />
       <div style={{flex: 6, paddingLeft: 15}}>
       <CasesDetail style={{ top: 0}}/>
       </div>
       </div> */}
 
-      <div id='menu' 
-      style={{width: '4rem', height: '4rem', borderRadius: '50%', position: 'fixed', textAlign: 'center'}}
-      className='m-s24 pointer purple-bg shadow-1'
-      onClick={toggleMenu}
-      >
-        <h2 style={{color: 'white', verticalAlign: 'middle', lineHeight: '1.5rem', fontSize:'1.5rem'}}>+</h2>
-      </div>
+        {/* <div id='hero-title'  className='row hero-title' >
+          <div className='f-s1 f-m2' />
+          <div className='col f-s10 f-m8 full-screen' >
+          
+            <div className='f-s1'  />
+            <div className='f-s2'  >
+              <div 
+                className='flex pink-border border-s6 border-solid p-s6'
+                style={{height: 200}}
+              >
+
+                <div 
+                className='full-hundred pink-bg text-align-center' >
+                  <h1  className='font-size-xl font-lineheight-xl lightgrey-col'>Steffos Portfolio</h1>
+                </div>
+              </div>
+            </div>
+            <div className='f-s1' />
+
+          </div>
+          <div className='f-s1 f-m2' />
+        </div> */}
+
+
+        <div id='menu' 
+          style={{width: '4rem', height: '4rem', borderRadius: '50%', position: 'fixed', textAlign: 'center'}}
+          className='m-s24 pointer purple-bg shadow-1'
+          onClick={toggleMenu}
+        >
+        
+          <h2 style={{color: 'white', verticalAlign: 'middle', lineHeight: '1.5rem', fontSize:'1.5rem'}}>+</h2>
+        </div>
+       
+
+     
+      
+      
+      
       </div>
       // <div className='row'>
       //   <div style={{backgroundColor: 'red', height: '30px'}} className='f-s1 f-xl12'>RAD 1</div>
       //   <div style={{backgroundColor: 'yellow', height: '30px'}} className='f-s1'>RAD 2</div>
       
-      // </div>
-      // <div id='MainContainer' className='col'>
-      //   <h1>Steffos Portfolio</h1>
+    // </div>
+    // <div id='MainContainer' className='col'>
+    //   <h1>Steffos Portfolio</h1>
 
-      //   <div id="InnerContainer">
-      //     <CasesList style={{flex: 4, top: 0}} />
-      //     <div style={{flex: 6, paddingLeft: 15}}>
-      //       <CasesDetail style={{ top: 0}}/>
-      //     </div>
-      //   </div>
-      // </div>
+    //   <div id="InnerContainer">
+    //     <CasesList style={{flex: 4, top: 0}} />
+    //     <div style={{flex: 6, paddingLeft: 15}}>
+    //       <CasesDetail style={{ top: 0}}/>
+    //     </div>
+    //   </div>
+    // </div>
     );
   }
 }

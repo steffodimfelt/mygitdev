5a023666bf3f828311aece3bf54b54cc7000cb96
Steffo Dimfelt <steffo.dimfelt@gmail.com>
2020-03-24 20:01:53 +0100

    Add client data

---
+++ src/reducers/ClientData.js
@@ -0,0 +1,72 @@
+export default () => {
+  return [
+    {
+      'id': 1,
+      'title': 'Eget projekt'
+    },
+    {
+      'id': 2,
+      'title': 'Ahlsell'
+    },
+    {
+      'id': 3,
+      'title': 'Hat On Lady'
+    },
+    {
+      'id': 4,
+      'title': 'Stockholmsmässan'
+    },
+    {
+      'id': 5,
+      'title': 'Ecoscoter'
+    },
+    {
+      'id': 6,
+      'title': 'Friskis&Svettis'
+    },
+    {
+      'id': 7,
+      'title': 'Johnnie Walker'
+    },
+    {
+      'id': 8,
+      'title': 'Löfbergs Lila'
+    },
+    {
+      'id': 9,
+      'title': 'Metallica'
+    },
+    {
+      'id': 10,
+      'title': 'Pontus Olgrim'
+    },
+    {
+      'id': 11,
+      'title': 'Scandic'
+    },
+    {
+      'id': 12,
+      'title': 'Skandia'
+    },
+    {
+      'id': 13,
+      'title': 'Stadium Promotion'
+    },
+    {
+      'id': 14,
+      'title': 'Swedbank'
+    },
+    {
+      'id': 15,
+      'title': 'Svenska Spel'
+    },
+    {
+      'id': 16,
+      'title': 'Telia'
+    },
+    {
+      'id': 17,
+      'title': 'TeliaSonera'
+    },
+  ];
+};

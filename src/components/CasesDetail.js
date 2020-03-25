1b7f40b8c6fb9ec5df503b3b33ed935ab6f9972d
Steffo Dimfelt <steffo.dimfelt@gmail.com>
2020-03-25 12:19:16 +0100

    case title

--- src/components/CasesDetail.js
+++ src/components/CasesDetail.js
@@ -21,7 +21,7 @@ class CasesDetail extends Component {
           <div className='row'>
             <div className='f-l2' />
             <div id='case-title' className='f-l8 text-align-center'>
-              <h1 id='case-title-header'>{headline}</h1>
+              <h1 className='pink-col'>{headline}</h1>
               <h6 className='pink-col' >{client}</h6>
             </div>
             <div className='f-l2' />
